e452dd266574473530e90bad8d31a5e2922a3888
Ningsong Shen <ningsongshen123@outlook.com>
2016-12-03 19:18:02 -0500

    Added ai, hardcoded

--- hello.py
+++ hello.py
@@ -3,6 +3,14 @@ from flask import Flask, render_template, request
 #testing
 app = Flask(__name__)
 
+
+dict = {
+    "joke" : "You are the joke",
+    "hi" : "Hey there",
+    "hello" : "Hi",
+    "How are": "Bad",    
+}
+
 @app.route("/")
 def hello():
     return render_template('home.html')
@@ -10,8 +18,11 @@ def hello():
 @app.route("/chat", methods=['POST'])
 def chat():
     input = request.form['userInput']
-    print(input)
-    return render_template('chat.html', input=input)
+    thing = input.strip().lower()
+    if thing in dict:
+        return render_template('chat.html', input=dict[thing])
+    else:
+        return render_template('chat.html', input="I can't do that yet")
 
 @app.route("/about")
 def about():
