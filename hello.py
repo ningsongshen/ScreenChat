7d89a2f03fda002f2edf37845d20d83fe201973e
Ningsong Shen <ningsongshen123@outlook.com>
2016-12-04 19:13:19 -0500

    Added a few replies, and now bot always replies!

--- hello.py
+++ hello.py
@@ -8,6 +8,7 @@ dict = {
         "hi" : "Hey there",
         "hello" : "Hi",
 		"hey" : "Haystack",
+        "yo" : "Yoyo",
         "how are you": "Bad",    
         "why" : "Why not",
         "hey siri" : "Hey Ugly",
@@ -20,6 +21,7 @@ dict = {
 		"gtg" : "Finally...",
 		"see you" : "No you don't",
 		"what's up" : "The sky",
+        "whats up" : "The ceiling",
 		"what is your favourite colour?" : "Glaucous",
 		"what do you eat?" : "Memes",
 		"screenbot" : "What?",
@@ -33,6 +35,7 @@ dict = {
 		"hello world" : "Goodbye universe",
 		"python" : "Don't talk about my guts. It's disgusting",
 		"rob ford" : "Feels bad man...",
+        "donald trump" : "I don't build walls",
 		"hackathon" : "Where I was born...",
 		"cipher" : "kevin macleod",
 		"darude sandstorm" : "Don't be rude",
@@ -53,13 +56,13 @@ def chat():
     input = request.form['userInput']
     thing = input.strip().lower()
     jokes = ["The environmental cost of finding a recycle bin for a water bottle is more than the environmental cost of just throwing the thing in the garbage.", "A QA analyst walks into a bar. Orders a beer. Orders 99999 beers. Orders -1 beers. Orders a sajdvhbj.", "A guy walks into a bar and asks for 1.4 root beers. The bartender says \"I'll have to charge you extra, that's a root beer float\". The guy says \"In that case, better make it a double."]
-     
+    not_sure = random.choice(list(dict.keys()))
     if "tell" in thing and "joke" in thing:
         return render_template('chat.html', input=jokes[random.randint(0,len(jokes) - 1)])
     elif thing in dict:        
         return render_template('chat.html', input=dict[thing])
     else:
-        return render_template('chat.html', input="I can't do that yet")
+        return render_template('chat.html', input=dict[not_sure])
     #for t in dict:        
     #    if dict.key() in thing:
     #        return render_template('chat.html', input=dict[t])
